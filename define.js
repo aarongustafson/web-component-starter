b4d94f952ecd4f0a62a8f4974fc282edbd7f2bac
Aaron Gustafson <aaron@easy-designs.net>
2025-12-08 09:13:26 -0800

    Harden define

--- define.js
+++ define.js
@@ -1,3 +1,18 @@
 import { ComponentNameElement } from './COMPONENT-NAME.js';
 
-customElements.define('COMPONENT-NAME', ComponentNameElement);
+export function defineComponentName(tagName = 'COMPONENT-NAME') {
+	const hasWindow = typeof window !== 'undefined';
+	const registry = hasWindow ? window.customElements : undefined;
+
+	if (!registry || typeof registry.define !== 'function') {
+		return false;
+	}
+
+	if (!registry.get(tagName)) {
+		registry.define(tagName, ComponentNameElement);
+	}
+
+	return true;
+}
+
+defineComponentName();
